192d80fc3c5a7feb536e050fb8f308634a6f2f9b
Olivier <47295195+sympact06@users.noreply.github.com>
2023-10-11 11:29:16 +0200

    yes

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import './assets/App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Moul&display=swap"
+      rel="stylesheet"
+    />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Document</title>
+    <div className="v5_140">
+      <div className="v4_93">
+        <div className="v3_47">
+          <div className="v4_94">
+            <div className="v5_137">
+              <div className="v5_138">
+                <div className="v5_139">
+                  <div className="v3_23" />
+                </div>
+              </div>
+            </div>
+            <div className="v4_96" />
+            <div className="v4_121" />
+            <div className="v5_125">
+              <div className="v5_124" />
+              <div className="v5_131" />
+              <div className="v5_130">
+                <div className="v5_123" />
+                <div className="v5_129" />
+              </div>
+            </div>
+            <div className="v4_92" />
+          </div>
+          <span className="v3_3">Your new friend with home delivery</span>
+          <span className="v3_8">
+            Online pet store, you can buy your friend right now. Act!
+          </span>
+          <div className="v3_11">
+            <div className="v3_81">
+              <div className="v3_82">
+                <div className="v3_9" />
+                <span className="v3_10">View friends</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="v5_128">
+        <div className="v3_90" />
+        <div className="v5_127">
+          <div className="v3_86">
+            <div className="v3_84" />
+            <div className="v3_79">
+              <span className="v3_64">+187 79 16 81 50</span>
+              <div className="v3_14">
+                <div className="v4_120">
+                  <div className="v4_119">
+                    <div className="v4_118">
+                      <div className="v3_12" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <span className="v3_15">HAPPY PET</span>
+              <div className="v3_54">
+                <span className="v3_32">Friends</span>
+                <span className="v3_31">Main</span>
+                <span className="v3_33">Service</span>
+                <span className="v3_34">Contacts</span>
+              </div>
+              <div className="v3_39" />
+              <span className="v3_38">1565 $</span>
+            </div>
+          </div>
+          <div className="v4_115" />
+        </div>
+      </div>
+      <div className="v4_97" />
+    </div>
+  </>
+  
+  )
+}
+
+export default App
--- src/components/Frame/SvgIcon7.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const SvgIcon7 = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path d='M12 12V6H11V12H5V13H11V19H12V13H18V12H12Z' fill='black' />
-  </svg>
-);
-const Memo = memo(SvgIcon7);
-export { Memo as SvgIcon7 };
